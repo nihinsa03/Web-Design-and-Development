cb7d54142194ebe9dce92c121e2ec3d40050256d
nihinsa03 <dnsbandara03@gmail.com>
2024-02-13 13:31:52 +0530

    Web Design and Development

---
+++ Web Design and Development/questions1.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which artist released the album Lover ?",
+    answer: "Taylor Swift",
+    options: [
+        "Taylor Swift",
+        "Beyoncé",
+        "Ed Sheeran",
+        "Ariana Grande"
+    ]
+    },
+    {
+        numb: 2,
+        question: "Which album won the Grammy Award for Album of the Year in 2020?",
+        answer: "When We All Fall Asleep, Where Do We Go?  by Billie Eilish",
+        options: [
+            "Golden Hour by Kacey Musgraves",
+            "When We All Fall Asleep, Where Do We Go?  by Billie Eilish",
+            "thank u, next by Ariana Grande",
+            "Cuz I Love You by Lizzo"
+        ]
+      },
+        {
+        numb: 3,
+        question: "Who released the album folklore in 2020?",
+        answer: "Taylor Swift",
+        options: [
+            "Harry Styles",
+            "Lady Gaga",
+            "Lana Del Rey",
+            "Taylor Swift"
+        ]
+      },
+        {
+        numb: 4,
+        question: "Which artist recently announced a world tour for 2023?",
+        answer: "Adele",
+        options: [
+            "Bruno Mars",
+            "Adele",
+            "Justin Bieber",
+            "Drake"
+            ]
+      },
+        {
+        numb: 5,
+        question: "Which album featured the hit song Drivers License",
+        answer: "SOUR by Olivia Rodrigo",
+        options: [
+            "SOUR by Olivia Rodrigo",
+            "EVERMORE by Taylor Swift", 
+            "FUTURE NOSTALGIA by Dua Lipa",
+            "POSITIONS by Ariana Grande"
+            ]
+      },
+      {
+        numb: 6,
+        question: "Who won the Best New Artist award at the 2021 Grammy Awards?",
+        answer: "Megan Thee Stallion",
+        options: [
+            "Megan Thee Stallion",
+            "Doja Cat",
+            "Phoebe Bridgers",
+            "Roddy Ricch"
+            ]
+      },
+      {
+        numb: 7,
+        question: "Which artist released the album Happier Than Ever in 2021?",
+        answer: "Billie Eilish",
+        options: [
+            "Billie Eilish",
+            "Halsey",
+            "Selena Gomez",
+            "Shawn Mendes"
+            ]
+      },
+      {
+        numb: 8,
+        question: "Who recently broke the record for the most-streamed album by a female artist in its first week?",
+        answer: "Taylor Swift",
+        options: [
+            "Cardi B",
+            "Adele",
+            "Taylor Swift",
+            "Beyoncé"
+            ]
+      },
+      {
+        numb: 9,
+        question: "Which album featured the song Blinding Lights ?",
+        answer: "AFTER HOURS by The Weeknd",
+        options: [
+            "AFTER HOURS by The Weeknd",
+            "FINE LINE by Harry Styles",
+            "FOLKLORE by Taylor Swift",
+            "CHROMATICA by Lady Gaga"
+            ]
+      },
+      {
+        numb: 10,
+        question: "Who became the youngest artist to win the Grammy Award for <b> Album of the Year?",
+        answer: "Billie Eilish",
+        options: [
+            "Billie Eilish",
+            "Taylor Swift",
+            "Lorde",
+            "Adele"
+            ]
+      },
+]
